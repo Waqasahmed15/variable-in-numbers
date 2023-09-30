2fd986ad57f0a234cf63b35a7058ee03c2bcf713
unknown <waqasahmedwikki15@gmail.com>
2023-09-30 19:35:51 +0500

    variable in numbers

---
+++ app.js
@@ -0,0 +1,25 @@
+
+// chapter 3 quastion 1
+let age = "I am 29 years old";
+alert(age);
+
+// chapter 3 quastion 2
+
+var visit = "You have visited this site 14 times.";
+alert(visit);
+
+
+// chapter 3 quastion 3
+var birth = "My Birth Day Year In 1993.\nDeta type of my declared variable is number";
+document.write(birth);
+
+
+
+
+// chapter 3 quastion 4
+
+var Vsname = "<br><br> Waqas Ahmed " ;
+var Vorder = "Ordered 5 T-shirt(s) ";
+var Vstore = "on XYZ Clothing store." ;
+document.write(Vsname+Vorder+Vstore);
+
